aa30a1753c807abaa3a368ce72f54057fc776d6e
Eduardo Alvarez <opticoedu@gmail.com>
2024-02-21 13:00:40 -0300

    morador.ts casi ok

--- src/entities/morador.ts
+++ src/entities/morador.ts
@@ -1,10 +1,16 @@
 import Pessoa from "./pessoa";
+import Apartamento from "./apartamento";
 
 class Morador {
-    private pessoa: Pessoa;
+    protected pessoa: Pessoa;
+    private apartamento: Apartamento;
     
-    constructor(pessoa: Pessoa){
+    constructor(pessoa: Pessoa, apartamento: Apartamento){
         this.pessoa = pessoa;
+        this.apartamento = apartamento;
+    }
+    public dataNascimento(): Date {
+        return this.pessoa.setDataNascimento();
     }
 }
 
