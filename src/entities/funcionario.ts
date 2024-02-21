51cb5d2e0bee4400ee48a450aa742f4b4808c782
Eduardo Alvarez <opticoedu@gmail.com>
2024-02-21 13:05:51 -0300

    funcionario OK

--- src/entities/funcionario.ts
+++ src/entities/funcionario.ts
@@ -1,11 +1,18 @@
 import Pessoa from "./pessoa";
+import Cargo from "./cargo";
 
 class Funcionario {
     private pessoa: Pessoa;
+    private cargo: Cargo;
+    private salario: number;
 
-    constructor(pessoa: Pessoa){
+    constructor(pessoa: Pessoa, cargo: Cargo, salario: number){
         this.pessoa = pessoa;
+        this.cargo = cargo;
+        this.salario = salario;
     }
 }
 
 export default Funcionario;
+
+//OK
